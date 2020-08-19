8d8b4424ad9afcc0adcfcb97cc59b82914037740
Lena Aiupova <e.ayupova@gmail.com>
2020-08-19 00:19:27 -0400

    added functions

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const battleship = () => {
+// ## Step 1: Create Players
+var shipNum = "4";
 
-  // ## Step 1: Create Players
 
 let playerOne = {
   name: prompt("playerOne - What is your first name?"),
-  shipCount: parseInt(4),
+  shipCount: parseInt(shipNum),
   gameBoard: [
     [0,0,0,0],
     [0,0,0,0],
@@ -15,7 +15,7 @@ let playerOne = {
 
 let playerTwo = {
   name: prompt("playerTwo - What is your first name?"),
-  shipCount: parseInt(4),
+  shipCount: parseInt(shipNum),
   gameBoard: [
     [0,0,0,0],
     [0,0,0,0],
@@ -24,141 +24,99 @@ let playerTwo = {
   ]
 }
 
+//function adding ships on the board
+
+const addShips = (player) => {
+let shipPlacePlayer = 0;
+
+	for (var i = 0; i < 4; i++) {
+
+	 let xCoordinate = Math.floor( Math.random() * (3 - 0 + 1) + 0);
+	 let yCoordinate = Math.floor( Math.random() * (3 - 0 + 1) + 0);
+
+	 console.log (xCoordinate, yCoordinate);
+
+	 if (player.gameBoard[xCoordinate][yCoordinate] === 0) {
+		player.gameBoard[xCoordinate][yCoordinate] = 1;
+		shipPlacePlayer +=1;
+	 } 
+	}
+//	return player.gameBoard;
+}
+
 //## Step 2: Randomly Add Ships to each Board
 
-//PlayerOne add ships to the existing array
-let shipPlacePlayerOne = 0;
-
-for (var i = 0; i < 4; i++) {
-
- let xCoordinateOne = Math.floor( Math.random() * (3 - 0 + 1) + 0);
- let yCoordinateOne = Math.floor( Math.random() * (3 - 0 + 1) + 0);
-
- console.log (xCoordinateOne, yCoordinateOne);
-
- if (playerOne.gameBoard[xCoordinateOne][yCoordinateOne] === 0) {
-    playerOne.gameBoard[xCoordinateOne][yCoordinateOne] = 1;
-    shipPlacePlayerOne +=1;
- } 
-}
-// show PlayerOne X and Y coordinates
+addShips(playerOne);
 console.log (JSON.stringify(playerOne.gameBoard));
 
-// PlyaerTwo add ships to the existing array
-let shipPlacePlayerTwo = 0;
-
-for (var i = 0; i < 4; i++) {
-
- let xCoordinateTwo = Math.floor( Math.random() * (3 - 0 + 1) + 0);
- let yCoordinateTwo = Math.floor( Math.random() * (3 - 0 + 1) + 0);
-
- console.log (xCoordinateTwo, yCoordinateTwo);
- 
- if (playerTwo.gameBoard[xCoordinateTwo][yCoordinateTwo] === 0) {
-    playerTwo.gameBoard[xCoordinateTwo][yCoordinateTwo] = 1;
-    shipPlacePlayerTwo +=1;
- } 
-}
-// show PlayerTwo X and Y coordinates
+addShips(playerTwo);
 console.log (JSON.stringify(playerTwo.gameBoard));
+
 // show info in objects : PlayerOne and PlayerTwo
 console.log(playerOne, playerTwo);
 
 //## Step 3: Start the Game Play
 
-let currentPlayer = playerOne.name;
-let message;
 let continueGame;
+let message;
+let currentPlayer = playerOne;
+let opponent = playerTwo;
+
+// choose the begginer
+let choice = Math.floor(Math.random() * (2 - 1 + 1) + 1);
+if (choice !== 1) {
+  currentPlayer = playerTwo;
+  opponent = playerOne;
+}
+
+// function defining whose turn
+
+const battleship = () => {
 
 while (!message) {  
 
-  // PlayerOne
-
-  if (currentPlayer === playerOne.name ) {
+//  if (currentPlayer === playerOne.name ) {
 
 //    ### Step 3a: Ask the PlayerOne to Choose Strike Coordinates
 
-    let xCoordinateStrike = +prompt(`${currentPlayer}, please choose an X coordinate to strike from 0 to 3`);
-    let yCoordinateStrike = +prompt(`${currentPlayer}, please choose an Y coordinate to strike from 0 to 3`);
+    let xCoordinateStrike = +prompt(`${currentPlayer.name}, please choose an X coordinate to strike from 0 to 3`);
+    let yCoordinateStrike = +prompt(`${currentPlayer.name}, please choose an Y coordinate to strike from 0 to 3`);
 
  // ### Step 3b: Determine if the Player Sink their Opponent's Ship
  // if the guesing coordinate is right, change the value from 1 to 0 and decrement the ship count
 
-    if (playerTwo.gameBoard[xCoordinateStrike][yCoordinateStrike] === 1) {
-          playerTwo.gameBoard[xCoordinateStrike][yCoordinateStrike] = 0;
-          playerTwo.shipCount -=1;
+    if (opponent.gameBoard[xCoordinateStrike][yCoordinateStrike] === 1) {
+          opponent.gameBoard[xCoordinateStrike][yCoordinateStrike] = 0;
+          opponent.shipCount -=1;
 // show info in object : PlayerTwo
-          console.log(playerTwo.name);
-          console.log (JSON.stringify(playerTwo.gameBoard));
+          console.log(opponent.name);
+          console.log (JSON.stringify(opponent.gameBoard));
           alert("Hint!");
 
-// if the guessing coordinate wrong, ask question and switch the players between each other         
-    } else {
+// ### Step 3c: Check if the Opponent has Any Ships Left  
+// if the guessing coordinate wrong, ask question and switch the players between each other 
+
+	} else {
       alert("Miss!");
 // confirm the continue of game    
-      continueGame = confirm("Would you like to continue?");
+       continueGame = confirm("Would you like to continue?");
         if (!continueGame) {
-          alert ("Stop Game");
-          return `Stop Game by ${currentPlayer}`;
-        }
-      currentPlayer = playerTwo.name;  
+         alert ("Stop Game");
+         return `Stop Game by ${currentPlayer.name}`;
+       }
+		[currentPlayer, opponent] = [opponent, currentPlayer]; 
     }
 
-// ### Step 3c: Check if the Opponent has Any Ships Left  
-// ## Step 4: End the Game Play  
-
+// ## Step 4: End the Game Play        
 // if the PlayerTwo ship count is 0, then PlayerOne win
 
-   if (playerTwo.shipCount === 0) {
-      message = playerOne.name;
-    } 
-  } 
-
-// PlayerTwo
-
-if (currentPlayer === playerTwo.name) {
-
-  //    ### Step 3a: Ask the Player to Choose Strike Coordinates
-  
-      let xCoordinateStrike = +prompt(`${currentPlayer}, please choose an X coordinate to strike`);
-      let yCoordinateStrike = +prompt(`${currentPlayer}, please choose an Y coordinate to strike`);
-
-   // ### Step 3b: Determine if the Player Sunk their Opponent's Ship
-   // if the guesing coordinate is right, change the value from 1 to 0 and decrement the ship count
-
-      if (playerOne.gameBoard[xCoordinateStrike][yCoordinateStrike] === 1) {
-            playerOne.gameBoard[xCoordinateStrike][yCoordinateStrike] = 0;
-            playerOne.shipCount -=1;
- // show info in object : PlayerOne
-            console.log(playerOne.name);
-            console.log (JSON.stringify(playerOne.gameBoard));
-            alert("Hint!");
-
-  // ### Step 3c: Check if the Opponent has Any Ships Left   
-  // if the guessing coordinate wrong, ask question and switch the players between each other  
-      }  else {
-        alert("Miss!");
-  // confirm the continue of game      
-        continueGame = confirm("Would you like to continue?");
-          if (!continueGame) {
-            alert ("Stop Game");
-            return `Stop Game by ${currentPlayer}`;
-          }
-        currentPlayer = playerOne.name;
-      }
-
-  // ## Step 4: End the Game Play  
-  // if the PlayerOne ship count is 0, then PlayerTwo win
-
-   if (playerOne.shipCount === 0) {
-    message = playerTwo.name;
-   }
-  }
-    
-}    
+	   if (opponent.shipCount === 0) {
+		message = currentPlayer.name;
+		} 
+}  
 // show info in objects : PlayerOne and PlayerTwo
   console.log(playerOne, playerTwo);
-  return `The winner is ${message}!`
+  return `The winner is ${message}!`;
 }
 
 const gameResult = battleship()
